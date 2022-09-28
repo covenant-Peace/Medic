36b0c26d91b2c750a63612cf761ac79a26911a43
Olubunmi <covenantp4@gmail.com>
2022-09-28 18:28:00 +0100

    commit

--- lib/get_started.dart
+++ lib/get_started.dart
@@ -6,12 +6,24 @@ class GetStarted extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double getH(double height){
+      return (height/812)*MediaQuery.of(context).size.height;
+    }
+    double getw(double width){
+      return (width/375)*MediaQuery.of(context).size.width;
+    }
     return Scaffold(
       backgroundColor: Colors.white,
       body: Stack(
         children: [
           Column(
-            children: [],
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: getH(150.24), left: getw(146.54)),
+                child: SvgPicture.asset('images/medic.svg'),
+              )
+
+            ],
           ),
           SvgPicture.asset('images/circle.svg')
         ],
